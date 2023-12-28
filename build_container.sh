01c750d6164d48df9d5d4fea58df1589268fc4ca
Daniel Nakhimovich <danielnak@tutanota.com>
2023-12-28 01:03:46 -0500

    include download of detic model in build script

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+if [ ! -f src/models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth ]; then
+	mkdir -p src/models
+	wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O src/models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
+fi
 docker build -t segment3d .
