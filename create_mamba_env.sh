9e22b7de4a9e045f135a97c3ce0f82c87b85bd8c
DanManN <d.nak@rutgers.edu>
2024-08-07 12:17:07 -0400

    add download to mamba script

--- create_mamba_env.sh
+++ create_mamba_env.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# download pretrained Detic model
+if [ ! -f src/models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth ]; then
+	mkdir -p src/models
+	wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O src/models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
+fi
+
 # install script created based on instructions from https://github.com/shiyoung77/OVIR-3D.git
 micromamba create -n ovir3d python=3.10 cuda \
 	opencv open3d pytorch-gpu torchvision torchmetrics \
